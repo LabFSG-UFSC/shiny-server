234af26ad7f3daaa0569af310108655be82992e2
Kevin <picknovak@gmai.com>
2026-05-11 14:48:42 -0300

    chore(app): trigger de homologacao do runner shiny

--- apps/teste-ci/app.R
+++ apps/teste-ci/app.R
@@ -3,7 +3,7 @@ library(shiny)
 ui <- fluidPage(
   titlePanel("LABFSG - Teste CI"),
   mainPanel(
-    h3("Deploy automatico via merge na main"),
+    h3("Deploy automatico via merge na main - trigger 2"),
     p("Data UTC:"),
     textOutput("ts")
   )
