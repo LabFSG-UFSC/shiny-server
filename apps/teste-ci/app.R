3a0c4fa39ef97112281eb426ccd775c04ee3383a
picknovak <picknovak@gmail.com>
2026-05-11 15:21:05 -0300

    chore(app): trigger deploy final apos auth ssh (#12)
    Co-authored-by: Kevin <picknovak@gmai.com>

--- apps/teste-ci/app.R
+++ apps/teste-ci/app.R
@@ -3,7 +3,7 @@ library(shiny)
 ui <- fluidPage(
   titlePanel("LABFSG - Teste CI"),
   mainPanel(
-    h3("Deploy automatico via merge na main - trigger 5"),
+    h3("Deploy automatico via merge na main - trigger 6"),
     p("Data UTC:"),
     textOutput("ts")
   )
